f571575f34fee833e2442f00d8084a187437e181
Loginov Ilya <dantes2104@gmail.com>
2019-04-06 22:48:51 +0400

    Edit to MainActivity

--- app/src/main/java/ru/liveproduction/victoria/activities/MainActivity.java
+++ app/src/main/java/ru/liveproduction/victoria/activities/MainActivity.java
@@ -12,7 +12,7 @@ import ru.liveproduction.victoria.R;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
