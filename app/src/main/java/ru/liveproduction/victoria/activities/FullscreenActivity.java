2c54a887eb89ded43541721d857027cc9ca35c55
Loginov Ilya <dantes2104@gmail.com>
2019-04-06 18:01:36 +0400

    Fix errors and refractor xml files

--- app/src/main/java/ru/liveproduction/victoria/activities/FullscreenActivity.java
+++ app/src/main/java/ru/liveproduction/victoria/activities/FullscreenActivity.java
@@ -1,18 +1,11 @@
-package ru.liveproduction.victoria;
+package ru.liveproduction.victoria.activities;
 
-import android.annotation.SuppressLint;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.text.TextPaint;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+
+import ru.liveproduction.victoria.R;
+
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
